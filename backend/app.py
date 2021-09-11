1da6e3941d5c47b97f6b59da336f8f5e33fedabe
Matthew Tzou <sonic55099@gmail.com>
2021-09-11 19:09:49 -0400

    updated function for pulling sales for a MY

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 import requests
-from src.data_helper import get_all_sales_json
+from src.data_helper import get_all_sales_json, get_sales
 from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
@@ -16,6 +16,20 @@ def get_all_sales():
 
     return jsonify({
         "allSales": dict
+    })
+
+# Returns JSON object of sales recorded for a specified model year
+# Returns: sales provided if available, -1 if not found
+@app.route('/api/v1/year-sales', methods=['POST'])
+def get_year_sales():
+    if not request.json or 'year' not in request.json or 'make' not in request.json or 'model' not in request.json:
+        print("ABORTING")
+        abort(400)
+    data = request.get_json()
+    sales_for_year = get_sales(int(data['year']), data['make'], data['model'])
+
+    return jsonify({
+        "sales": sales_for_year
     })
 
 
