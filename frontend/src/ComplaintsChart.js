3a0f1de0b2c2836077036fc0781208961ed9d504
Matthew Tzou <sonic55099@gmail.com>
2021-12-27 15:04:22 -0500

    updated: ComplaintsChart

--- frontend/src/ComplaintsChart.js
+++ frontend/src/ComplaintsChart.js
@@ -11,54 +11,61 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Area, AreaChart,
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import './CircularProgressbar.css';
-import abs from './resources/abs.png';
-import airbag from './resources/airbag.png';
-import battery from './resources/battery.png';
-import cruise from './resources/cruise.png';
-import engine from './resources/engine.png';
-import gas from './resources/gas.png';
-import seatbelt from './resources/seatbelt.png';
-import steering from './resources/steering.png';
-import tire from './resources/tire.png';
-import highlander from './resources/highlander.jpg';
-import ComplaintChart from './ComplaintYearChart.js';
-import SearchBar from './SearchBar.js';
-import wrench from './resources/general.png';
+
 
 function getMaxComplaints(jsonData) {
     console.log(jsonData);
-    console.log(jsonData["complaints"]);
-    let dataAsJson = JSON.parse(jsonData);
-    for (let i = 0; i < dataAsJson.length; i++) {
-        console.log(dataAsJson["complaints"][i]);
+    // let dataAsJson = JSON.parse(jsonData);
+    let maxValue = 0;
+    for (let i = 0; i < jsonData.length; i++) {
+        if (parseInt(jsonData[i]["complaints"]) > maxValue) {
+            maxValue = jsonData[i]["complaints"];
+        }
     }
+    return maxValue;
+    /*
+    let maxValue = dataAsJson["complaints"][0];
+    for (let i = 0; i < dataAsJson.length; i++) {
+        if (dataAsJson["complaints"][i] > maxValue) {
+            maxValue = dataAsJson["complaints"][i];
+        }
+    }
+    console.log(maxValue);
+    return maxValue;
+    */
+
 }
 
 function ComplaintsChart() {
     const [complaintsChartData, setComplaintsChartData] = useState({});
+    const [maxValue, setMaxValue] = useState(0);
 
     useEffect(async () => {
         await Axios.post("/api/v1/recharts-complaints", { "year": "2014", "make": "hyundai", "model": "sonata" }).then((response) => {
             setComplaintsChartData(response.data.data);
-            getMaxComplaints(response.data.data);
+            setMaxValue(parseInt(getMaxComplaints(response.data.data)));
         });
     }, []);
     return (
-        <ResponsiveContainer width="95%" height={300}>
-            <AreaChart width={800} height={250} data={complaintsChartData} margin={{ top: 0, right: 20, bottom: 30, left: 25 }} class="charts">
-                <defs>
+        <div>
+            <ResponsiveContainer width="95%" height={300}>
+                <AreaChart width={800} height={250} data={complaintsChartData} margin={{ top: 0, right: 20, bottom: 30, left: 25 }} class="charts">
+                    <defs>
 
-                </defs>
-                <XAxis dataKey="year">
-                    <Label value="Year" offset={-20} position="insideBottom" />
+                    </defs>
+                    <XAxis dataKey="year">
+                        <Label value="Year" offset={-20} position="insideBottom" />
 
-                </XAxis>
-                <YAxis domain={[0, parseInt('dataMax')]} label={{ value: 'Complaints', angle: -90, offset: 0, position: 'insideLeft' }} />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Area type="monotone" dataKey="complaints" stroke="#BA0C2F" fillOpacity={0.5} fill="#BA0C2F" />
-            </AreaChart>
-        </ResponsiveContainer>
+                    </XAxis>
+                    <YAxis domain={[0, maxValue]} label={{ value: 'Complaints', angle: -90, offset: 0, position: 'insideLeft' }} />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="complaints" stroke="#BA0C2F" fillOpacity={0.5} fill="#BA0C2F" />
+                </AreaChart>
+            </ResponsiveContainer>
+            <h1>{typeof maxValue} {maxValue}</h1>
+        </div>
+
     );
 }
 
